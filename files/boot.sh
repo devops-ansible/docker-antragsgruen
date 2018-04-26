6bc565df5c7aa58c6f75e2127ac66c6bb13bf8df
Martin Winter <git@macwinnie.me>
2018-04-26 14:40:31 +0200

    undo latest changes and repair boot.sh

--- files/boot.sh
+++ files/boot.sh
@@ -12,16 +12,16 @@ if [ ! -z ${SMTP_HOST+x} ] && [ ! -z ${SMTP_FROM+x} ] && [ ! -z ${SMTP_PASS+x} ]
     j2 /templates/msmtprc.j2 > /etc/msmtprc
 fi
 
-chown -R $WORKINGUSER:$WORKINGGROUP $WORKINGDIR
+chown -R $WORKINGUSER:$WORKINGGROUP $APACHE_WORKDIR
 
-cd $WORKINGDIR
+cd $APACHE_WORKDIR
 sudo -u $WORKINGUSER composer global require "fxp/composer-asset-plugin:1.3.1"
 sudo -u $WORKINGUSER composer install --prefer-dist
 sudo -u $WORKINGUSER npm install
 sudo -u $WORKINGUSER npm run build
 
-if [ -f $WORKINGDIR/config/config.json ]; then
-    sudo -u $WORKINGUSER expect -c "spawn $WORKINGDIR/yii migrate; expect -re \"Apply the above migrations?.*\"; send 'yes\r\n';"
+if [ -f $APACHE_WORKDIR/config/config.json ]; then
+    sudo -u $WORKINGUSER expect -c "spawn $APACHE_WORKDIR/yii migrate; expect -re \"Apply the above migrations?.*\"; send 'yes\r\n';"
 else
     sudo -u $WORKINGUSER touch $WORKINGDIR/config/INSTALLING
 fi
