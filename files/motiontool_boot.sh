a39d138a53b1acd1d4a677235494896fdcf8748a
Martin Winter <dev@macwinnie.me>
2019-08-31 17:55:36 +0200

    fix failing expect for auto migration

--- files/motiontool_boot.sh
+++ files/motiontool_boot.sh
@@ -3,14 +3,21 @@
 ###
 
 cd $APACHE_WORKDIR
-sudo -u $WORKINGUSER composer global require fxp/composer-asset-plugin hirak/prestissimo
+sudo -u $WORKINGUSER composer global require "fxp/composer-asset-plugin:1.3.1"
 sudo -u $WORKINGUSER composer install --prefer-dist
 sudo -u $WORKINGUSER npm install
 sudo -u $WORKINGUSER npm run build
 
 if [ -s $APACHE_WORKDIR/config/config.json ]; then
     
-    sudo -u $WORKINGUSER expect -c "spawn $APACHE_WORKDIR/yii migrate; expect -re \"Apply the above migrations?.*\"; send 'yes\r\n';"
+    sudo -u $WORKINGUSER printf "
+    set timeout -1
+    spawn $APACHE_WORKDIR/yii migrate
+    expect {
+       \"Apply the above\" { send \"yes\\\n\"; }
+    }
+    expect eof
+    " | expect
 
     if [ "$LATEX_ENABLE" == "yes" ]; then
       php /latex.php $APACHE_WORKDIR yes
