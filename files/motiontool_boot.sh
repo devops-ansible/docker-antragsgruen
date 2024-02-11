cdbbbe9eefbf60b3d574bbf9652f6215cd9404af
macwinnie <dev@macwinnie.me>
2024-02-11 12:27:18 +0100

    run `npm` stuff on bootup only if `node_modules` does not exist
    Accepting #4 and preventing `npm` running each and every time on Container start.

--- files/motiontool_boot.sh
+++ files/motiontool_boot.sh
@@ -6,10 +6,17 @@ cd "${APACHE_WORKDIR}"
 
 chown -R "${WORKINGUSER}" "${APACHE_WORKDIR}"
 
-sudo -u "${WORKINGUSER}" composer install --prefer-dist
-sudo -u "${WORKINGUSER}" npm install
-sudo -u "${WORKINGUSER}" npm run build
-# sudo -u "${WORKINGUSER}" gulp
+# only run `composer install` if `vendor` not present
+if [ ! -d "./vendor" ]; then
+    sudo -u "${WORKINGUSER}" composer install --prefer-dist
+fi
+
+# only run `npm` stuff if `node_modules` are not present
+if [ ! -d "./node_modules" ]; then
+    sudo -u "${WORKINGUSER}" npm install
+    sudo -u "${WORKINGUSER}" npm run build
+    # sudo -u "${WORKINGUSER}" gulp
+fi
 
 if [ -s "${APACHE_WORKDIR}/config/config.json" ]; then
     
